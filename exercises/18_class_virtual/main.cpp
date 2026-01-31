30da2d071ae42df2f03517af7e7916a11d7525c6
huayunna3 <2439429638@qq.com>
2026-01-31 16:14:36 +0800

    作业完成版

--- exercises/18_class_virtual/main.cpp
+++ exercises/18_class_virtual/main.cpp
@@ -6,7 +6,7 @@ struct A {
     virtual char virtual_name() const {
         return 'A';
     }
-    char direct_name() const {
+    virtual char direct_name() const {
         return 'A';
     }
 };
@@ -15,7 +15,7 @@ struct B : public A {
     char virtual_name() const override {
         return 'B';
     }
-    char direct_name() const {
+    char direct_name() const override {
         return 'B';
     }
 };
@@ -24,12 +24,12 @@ struct C : public B {
     char virtual_name() const final {
         return 'C';
     }
-    char direct_name() const {
+    char direct_name() const override {
         return 'C';
     }
 };
 struct D : public C {
-    char direct_name() const {
+    char direct_name() const override {
         return 'D';
     }
 };
@@ -42,38 +42,38 @@ int main(int argc, char **argv) {
     C c;
     D d;
 
-    ASSERT(a.virtual_name() == '?', MSG);
-    ASSERT(b.virtual_name() == '?', MSG);
-    ASSERT(c.virtual_name() == '?', MSG);
-    ASSERT(d.virtual_name() == '?', MSG);
-    ASSERT(a.direct_name() == '?', MSG);
-    ASSERT(b.direct_name() == '?', MSG);
-    ASSERT(c.direct_name() == '?', MSG);
-    ASSERT(d.direct_name() == '?', MSG);
+    ASSERT(a.virtual_name() == 'A', MSG);
+    ASSERT(b.virtual_name() == 'B', MSG);
+    ASSERT(c.virtual_name() == 'C', MSG);
+    ASSERT(d.virtual_name() == 'C', MSG); // C is final, so D inherits C's virtual_name
+    ASSERT(a.direct_name() == 'A', MSG);
+    ASSERT(b.direct_name() == 'B', MSG);
+    ASSERT(c.direct_name() == 'C', MSG);
+    ASSERT(d.direct_name() == 'D', MSG);
 
     A &rab = b;
     B &rbc = c;
     C &rcd = d;
 
-    ASSERT(rab.virtual_name() == '?', MSG);
-    ASSERT(rbc.virtual_name() == '?', MSG);
-    ASSERT(rcd.virtual_name() == '?', MSG);
-    ASSERT(rab.direct_name() == '?', MSG);
-    ASSERT(rbc.direct_name() == '?', MSG);
-    ASSERT(rcd.direct_name() == '?', MSG);
+    ASSERT(rab.virtual_name() == 'B', MSG);
+    ASSERT(rbc.virtual_name() == 'C', MSG);
+    ASSERT(rcd.virtual_name() == 'C', MSG);
+    ASSERT(rab.direct_name() == 'B', MSG);
+    ASSERT(rbc.direct_name() == 'C', MSG);
+    ASSERT(rcd.direct_name() == 'D', MSG);
 
     A &rac = c;
     B &rbd = d;
 
-    ASSERT(rac.virtual_name() == '?', MSG);
-    ASSERT(rbd.virtual_name() == '?', MSG);
-    ASSERT(rac.direct_name() == '?', MSG);
-    ASSERT(rbd.direct_name() == '?', MSG);
+    ASSERT(rac.virtual_name() == 'C', MSG);
+    ASSERT(rbd.virtual_name() == 'C', MSG);
+    ASSERT(rac.direct_name() == 'C', MSG);
+    ASSERT(rbd.direct_name() == 'D', MSG);
 
     A &rad = d;
 
-    ASSERT(rad.virtual_name() == '?', MSG);
-    ASSERT(rad.direct_name() == '?', MSG);
+    ASSERT(rad.virtual_name() == 'C', MSG);
+    ASSERT(rad.direct_name() == 'D', MSG);
 
     return 0;
 }
